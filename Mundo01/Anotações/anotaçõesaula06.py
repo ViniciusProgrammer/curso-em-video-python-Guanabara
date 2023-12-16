94e0feeee3f30dd8e9e132faed6605dd03e1bdc8
Vincios Davi <viniprogrammer@hotmail.com>
2023-12-16 11:01:00 -0300

    anotações da aula06

---
+++ Mundo01/Anotações/anotaçõesaula06.py
@@ -0,0 +1,6 @@
+'''
+int = 7, -4, 0, 9875 = são números inteiros 
+float = 4.5, 0.076, -15.223, 7.0 = são números reais oi de ponto flutuante
+bool = True, False = são valores lógicos ou booleanos 
+str = 'Olá', '7.5', '' = são valores caracteres ou strings 
+'''
