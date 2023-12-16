7271cedb37312eca92d5de501fc41b645d4690c2
Vincios Davi <viniprogrammer@hotmail.com>
2023-12-16 10:59:23 -0300

    anotações da aula02

---
+++ Mundo01/Anotações/anotaçõesaula02.py
@@ -0,0 +1,6 @@
+
+'''nome = input('Digite o seu nome: ')
+print('Olá', {}, 'Seja bem vindo!', 'é um prazer te conhecer!'.format(nome)
+
+Essa formatação também funciona, pela máscara com o .format()
+'''
