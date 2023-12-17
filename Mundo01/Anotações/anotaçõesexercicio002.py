e4aa2df2c46ad5a0afbb321c0a05dc142dd8ddea
Vincios Davi <viniprogrammer@hotmail.com>
2023-12-17 14:55:17 -0300

    Arquivo deletado

--- Mundo01/Anotações/anotaçõesexercicio002.py
+++
@@ -1,6 +0,0 @@
-
-'''nome = input('Digite o seu nome: ')
-print('Olá', {}, 'Seja bem vindo!', 'é um prazer te conhecer!'.format(nome)
-
-Essa formatação também funciona, pela máscara com o .format()
-'''
