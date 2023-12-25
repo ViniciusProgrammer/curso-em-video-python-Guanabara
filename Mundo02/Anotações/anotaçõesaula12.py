b604ef4a8fda0210d520be4bb1f944be067e7303
Vincios Davi <viniprogrammer@hotmail.com>
2023-12-25 14:55:32 -0300

    Anotações da aula 12

---
+++ Mundo02/Anotações/anotaçõesaula12.py
@@ -0,0 +1,13 @@
+'''
+
+Estruturas cindicionais aninhadas
+
+O comoando elif,
+
+pode-se ter quantos elifs quiser, mas só um if e um else oou nenhum else po rexemplo
+
+
+salario  ------------ 100%
+prestacao  ---------- 30%
+salario * 30 = valor presatacao * 100
+'''
