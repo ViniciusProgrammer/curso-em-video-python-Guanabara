a7591db91d9fbfd9f2f83d4322c3ee670da4fb8e
Vincios Davi <viniprogrammer@hotmail.com>
2023-12-29 16:27:00 -0300

    Anotações da aula 13

---
+++ Mundo02/Anotações/anotaçõesaula13.py
@@ -0,0 +1,30 @@
+'''
+Trabalhando como o laço de repetição for:
+
+for c in range(0, 6): é uma estrutura de repetição com variável de controle
+esse é um bom exemplo a ser dado para o aluno, pois ele pode ver a variável de controle
+
+ofr c in range(1, 10):
+    passo
+passo
+
+O comando for vai de onde o programador setar até o número anterior ao último número setado
+
+exemplo:
+for c in range(0, 6):
+    print(c)
+
+print('FIM')
+
+ELe vai imprimir de 0 a 5, pois o 6 é o último número setado, mas ele não é impresso
+
+
+outro exemplo:
+
+s = 0
+for c in range(0, 4):
+    n = int(input('Digite um valor: '))
+    s += n  
+print('O somatório de todos os valores foi {}'.format(s))
+
+'''
